2fd107bb41140ea2266c0d30dde52b7fc0fecaff
Nitesh Oswal <nit.oswal@gmail.com>
2015-12-08 22:21:00 +0530

    Work on the utility over the already specified specification

--- circular.js
+++ circular.js
@@ -1,16 +1,71 @@
 /**
- * Circular.js V1.0
- * Author: Nitesh Oswal
+ * The wrapper which allows the module to work in different environment, CommonJS-like envs like Node, AMD loaders such as Require.js, and ofcourse can be used globally in browser
+ * @param  {function} scope   The scope or the root, it is the environment we're going to glue our logic to
+ * @param  {function} utility The Circular.js utility itself
+ * @return {undefined}
  */
+;(function(scope, utility) {
 
-;(function(scope, factory) {
+    if(typeof define === "function" && define.amd) {
+        //For AMD loaders & lovers
+        define(['circular'], function(circular) {
 
-    this.circular = factory;
-
-})(this, function(name) { //I always forget the techincal name for this, IIFE - Immediately-Invoked Function Expression
-
-    return function(key, value) {
-        return value;
+            return (scope.circular = utility);
+        });
+    } else if(typeof exports === "object") {
+        //for Node
+        module.exports = utility;
+    } else {
+        //for browsers :heart:
+        scope.circular = utility;
     }
 
+    this.circular = utility;
+
+})(this, function(ref, options) {
+
+    if(typeof ref === "object" || typeof ref === "undefined") {
+        options = ref || {};
+        ref = "[circular Circular]";
+    }
+
+    var defaults = {
+        stringifyFunctions: true, //do you want to stringify functions, I really don't know why, but I don't wanna
+        keep: [], //much like the replacer's already known functionality, of filtering objects using arrays
+        discard: []
+    };
+
+    //Extend defaults with options
+    if(typeof Object.assign === "function") { //needs ES6, why not!
+        defaults = Object.assign(defaults, options); 
+    } else { //fallback -_- didn't wanted the ES6 polyfill, just for my use case
+        for(option in options) { //copy options into defaults
+            defaults[option] = options[option];
+        }
+    }
+
+    var traversed = [];
+    return function(key, value) {
+
+        if(defaults.keep.length > 0 && defaults.keep.indexOf(key) == -1) return;
+        if(defaults.discard.length > 0 && defaults.discard.indexOf(key) > -1) return;
+
+        if(typeof value === "function" && defaults.stringifyFunctions) {
+            return value.toString(); //stringify functions
+        }
+
+        if(!value || typeof value !== "object") {
+            return value; //let those booleans and strings and other things pass
+        }
+
+        if(traversed.indexOf(value) > -1) { //check if the "value" was present in the traversed stack
+            if(typeof ref === "function") return ref(value);
+            return ref;
+        }
+
+        traversed.push(value); //we've traversed "this" lol, get it? no? k..
+
+        return value; //who are we! utilities! 
+                      //What do we do! return that value!
+    }
 });
